a3939c1a638baba1387213bbc148e6100fcbb098
einbone921 <einbone921@gmail.com>
2020-12-28 04:25:42 +0000

    [Update]homes#topをroot_pathに設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
 
     protected
 
+    def after_sign_out_path_for(resource_or_scope)
+      root_path
+    end
+
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys[:name])
+      devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'homes/top'
+  root 'homes#top'
   devise_for :users, controllers: {
-    sessions: 'users/sessions', 
+    sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
 end
